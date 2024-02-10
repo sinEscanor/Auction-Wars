12abff6d1864730738e0aff1dd9f9a3d45708b09
Ankit <ankitkumar19041@gmail.com>
2024-02-10 18:46:24 +0530

    fix

--- src/utils/authutils.ts
+++ src/utils/authutils.ts
@@ -12,7 +12,7 @@ export const getToken = () => {
 export const removeCookie = ()=>{
   const cookies = document.cookie.split("; ");
   for (const cookie of cookies) {
-    const [name,value] = cookie.split("=");
+    const [name] = cookie.split("=");
     if (name === "jwtToken") {
       document.cookie = "jwtToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     }
