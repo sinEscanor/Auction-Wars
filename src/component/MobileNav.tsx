5f3aec6f41679b68986474a091f365e4d4609e1c
Ankit <ankitkumar19041@gmail.com>
2024-03-12 11:19:25 +0530

    nav fix

--- src/component/MobileNav.tsx
+++ src/component/MobileNav.tsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-interface VisibleI{
-    isVisible: boolean,
-    setIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+interface VisibleI {
+  isVisible: boolean;
+  setIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const MobileNav: React.FC<VisibleI> = ({isVisible, setIsVisible}) => {
-    const navigate = useNavigate()
-    const user = useSelector((state : any)=>state.Authenticate.user)
+const MobileNav: React.FC<VisibleI> = ({ isVisible, setIsVisible }) => {
+  const navigate = useNavigate();
+  const user = useSelector((state: any) => state.Authenticate.user);
   return (
     <div
-      className={`fixed w-full inset-0 bg-black bg-opacity-50  duration-300 transition-transform z-40 flex-shrink-0 top-0 ${
-        isVisible ?  "-translate-x-full absolute" : "fixed xl:sticky"
+      className={`fixed w-full  duration-300 bg-black bg-opacity-40   transition-transform z-40 flex-shrink-0 top-0 ${
+        isVisible ? "left-0" : " -translate-x-full"
       }`}
       onClick={() => setIsVisible(!isVisible)}
     >
       <div
-        className="fixed text-white flex flex-col  text-xl font-light font-roboto justify-between bg-gradient-to-r from-[#210a0a] via-[#271d1d] to-[#170808]  h-full w-[60vw] p-4 shadow-md top-0 left-0"
-      
-        onClick={(e) => e.stopPropagation()}
+        className=" text-white flex flex-col  text-xl font-light font-roboto justify-between bg-gradient-to-r from-[#210a0a] via-[#271d1d] to-[#170808]  h-[100vh] w-[60vw] p-4 shadow-md "
+        // onClick={(e) => e.stopPropagation()}
       >
         <ul className="p-2 flex  flex-col space-y-6">
           <li className="mb-6 text-xl font-semibold">
@@ -39,17 +37,28 @@ const MobileNav: React.FC<VisibleI> = ({isVisible, setIsVisible}) => {
           </li>
 
           <li className="hover:pl-2 duration-200">
-          { 
-            user ?(  <button onClick={()=>navigate('/userinfo')} className='bg-gradient-to-r from-[#EC9F05] to-[#FF4E00]  rounded-lg p-2 px-4'> {user.user.name}</button>):
-              (<button onClick={()=> navigate('/login')} className='bg-gradient-to-r from-[#20BF55] to-[#01BAEF] bg-clip-text bg-transparent rounded-lg p-2 px-4 '> Login</button>)
-            } 
+            {user ? (
+              <button
+                onClick={() => navigate("/userinfo")}
+                className="bg-gradient-to-r from-[#EC9F05] to-[#FF4E00]  rounded-lg p-2 px-4"
+              >
+                {" "}
+                {user.user.name}
+              </button>
+            ) : (
+              <button
+                onClick={() => navigate("/login")}
+                className="bg-gradient-to-r from-[#20BF55] to-[#01BAEF] bg-clip-text bg-transparent rounded-lg p-2 px-4 "
+              >
+                {" "}
+                Login
+              </button>
+            )}
           </li>
-   
         </ul>
       </div>
     </div>
   );
-  
-}
+};
 
-export default MobileNav
+export default MobileNav;
